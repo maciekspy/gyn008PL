34e0307d5f87a01731d536ecca15f95fa54a8deb
Maciek <maciekspy@gmail.com>
2017-07-13 23:21:26 +0200

    Solution for mission 008PL

---
+++ Drone/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Drone
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(392, 379);
+            this.panel1.TabIndex = 10;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button7);
+            this.panel2.Controls.Add(this.button6);
+            this.panel2.Controls.Add(this.button5);
+            this.panel2.Controls.Add(this.button4);
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(392, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(135, 379);
+            this.panel2.TabIndex = 0;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(24, 41);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(99, 23);
+            this.button7.TabIndex = 12;
+            this.button7.Text = "Save map to png";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(24, 12);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(99, 23);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "Load cached files";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button5.Location = new System.Drawing.Point(21, 136);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(48, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Left";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.Location = new System.Drawing.Point(75, 136);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(48, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Right";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(44, 165);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(54, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Down";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(54, 107);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(33, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Up";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(527, 379);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Name = "Form1";
+            this.Text = "Drone - Misja 008";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ Drone/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Drone
+{
+    public partial class Form1 : Form
+    {
+        private static string data_dir = @"data\";
+        private static string base_url = "http://gynvael.coldwind.pl/misja008_drone_io/scans/";
+
+        private int cx = -1, cy = - 1;
+
+        private Bitmap map;
+
+        private static float pixPerMeter = 5;
+        private static float widthMeter = 1000;
+        private static float heightMeter = 900;
+
+        private string[,] urls;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            urls = new string[(int)widthMeter, (int)heightMeter];
+
+
+            map = new Bitmap((int)Math.Floor(widthMeter * pixPerMeter), 
+                (int)Math.Floor( heightMeter * pixPerMeter));
+
+
+            using (Graphics graph = Graphics.FromImage(map))
+            {
+                Rectangle ImageSize = new Rectangle(0, 0, map.Width, map.Height);
+                graph.FillRectangle(Brushes.White, ImageSize);
+            }
+
+            panel1.AutoScroll = true;
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox1.Image = map;
+
+
+            string start = "68eb1a7625837e38d55c54dc99257a17.txt";
+            scan(start);
+
+        }
+
+        private void set_point(int x, int y, Color c)
+        {
+            try
+            {
+                map.SetPixel(x, y, c);
+                map.SetPixel(x + 1, y, c);
+                map.SetPixel(x - 1, y, c);
+                map.SetPixel(x, y + 1, c);
+                map.SetPixel(x, y - 1, c);
+            }
+            catch (Exception ex) { }
+        }
+
+        private string get_link(string l)
+        {
+            if (l == "not possible") return null;
+            return l;
+        }
+
+        private void parse_scan(string s)
+        {
+            string[] lines = s.Split('\n');
+            if (lines.Length != 42)
+                return;
+
+            cx = int.Parse(lines[1].Split(' ')[0]);
+            cy = int.Parse(lines[1].Split(' ')[1]);
+
+            int x = (int)Math.Round(cx * pixPerMeter);
+            int y = (int)Math.Round(cy * pixPerMeter);
+
+            set_point(x, y, Color.Red);
+
+            for (int i = 0; i < 36; i++)
+            {
+                float dist;
+                if (float.TryParse(lines[2 + i].Replace('.', ','), out dist))
+                {
+                    int x2 = x + (int)Math.Round(dist * pixPerMeter * Math.Sin(i * Math.PI / 18.0));
+                    int y2 = y - (int)Math.Round(dist * pixPerMeter * Math.Cos(i * Math.PI / 18.0));
+                    set_point(x2, y2, Color.Black);
+                }
+            }
+
+            //east
+            //west
+            //south
+            //north
+            try
+            {
+                urls[cx + 1, cy] = get_link(lines[38].Substring(11));
+                urls[cx - 1, cy] = get_link(lines[39].Substring(11));
+                urls[cx, cy - 1] = get_link(lines[40].Substring(12));
+                urls[cx, cy + 1] = get_link(lines[41].Substring(12));
+            }
+            catch (Exception ex) { }
+
+        }
+
+        private bool scan(string file)
+        {
+            if (file == null) { 
+                
+                Debug.WriteLine("No possible");
+                return false;
+            }
+
+            string s;
+            if (File.Exists(data_dir + file))
+            {
+                s = File.ReadAllText(data_dir + file);
+            } else {
+                using (WebClient client = new WebClient())
+                {
+                    s = client.DownloadString(base_url + file);
+                }
+                Debug.WriteLine("Downloaded: " + file);
+                File.WriteAllText(data_dir + file, s);
+            }
+
+            parse_scan(s);
+
+            pictureBox1.Image = map;
+            pictureBox1.Refresh();
+            Debug.WriteLine("Loaded: " + file);
+            return true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            scan(urls[cx, cy - 1]);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            scan(urls[cx, cy + 1]);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            scan(urls[cx + 1, cy]);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            scan(urls[cx - 1, cy]);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo d = new DirectoryInfo(data_dir);
+
+            foreach (var file in d.GetFiles("*.txt"))
+            {
+                Debug.WriteLine(file);
+                string s = File.ReadAllText(data_dir + file);
+                parse_scan(s);
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            MouseEventArgs me = (MouseEventArgs)e;
+
+            int x = (int)Math.Round(me.Location.X / pixPerMeter);
+            int y = (int)Math.Round(me.Location.Y / pixPerMeter);
+
+            if (urls[x, y] != null)
+            {
+                scan(urls[x, y]);
+            }
+            else
+            {
+                bool ret = false;
+                do
+                {
+                    int dx = x - cx;
+                    int dy = cy - y;
+
+                    if (dx == 0 && dy == 0) return;
+
+                    if (Math.Abs(dx) > Math.Abs(dy))
+                    {
+                        ret = scan(urls[cx + Math.Sign(dx), cy]);
+                    }
+                    else
+                    {
+                        ret = scan(urls[cx, cy + Math.Sign(dy)]);
+                    }
+
+                } while (ret);
+            }
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            map.Save("map.png", System.Drawing.Imaging.ImageFormat.Png);
+        }
+
+    }
+}
